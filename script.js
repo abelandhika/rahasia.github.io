2ebb47514cdbb96e2d496d5fbd11f93e67cacf09
abelandhika <abelsyahroni@gmail.com>
2021-11-18 17:33:38 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// Cara menulis JavaScript
+
+var x = 10;
+console.log('Hello World');
+console.log('isi dari variabel x adalah ' + x);
+
+// POPUP BOX 
+// Alert
+// prompt
+
+
+alert('hai:D');
+var nama = prompt('kamu siapa');
+alert('ohhh hai ' + nama);
+nama = prompt('gimana kabar kamu');
+alert('syukurlah jika kamu baik');
+alert('jika kamu sedang ada masalah,');
+alert('sabar ya, tuhan tau yang terbaik buat kamu')
+alert('your is strong')
